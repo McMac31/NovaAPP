d146ca279802dc5811582c3cee86ee661ee83215
ikMendez <ikyemendez24@lhusurbil.eus>
2025-11-03 12:41:59 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NovaAPP',
+  webDir: 'www'
+};
+
+export default config;
